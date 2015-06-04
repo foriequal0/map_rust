b9c3c792587ef77540f14bffa1bac04fadaad4e5
SeongChan Lee <foriequal@gmail.com>
2015-06-05 04:20:44 +0900

    OptionHelper test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+mod option_helper;
+
 use std::thread;
+use option_helper::*;
 
 fn main() {
+    let opts = OptionHelper::new();
+    match opts.parse_opts() {
+        Some(x) => println!("YAY"),
+        None => println!("{}", opts.get_usage()),
+    }
 }
