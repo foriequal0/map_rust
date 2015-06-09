ed601715379a905c1da970bd33886d166d291e22
SeongChan Lee <foriequal@gmail.com>
2015-06-09 10:26:51 +0900

    join_all accepts generic lists

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,17 @@ mod slave;
 use std::thread;
 use option_helper::*;
 
-trait JoinAll {
-    fn join_all(self);
+trait JoinAll<T> {
+    fn join_all(self) -> Vec<thread::Result<T>>;
 }
 
-impl JoinAll for Vec<thread::JoinHandle<()>> {
-    fn join_all(self) {
-        for v in self {
-            v.join()
-        }
+impl<T, C: IntoIterator> JoinAll<T> for C
+    where C::Item: Into<thread::JoinHandle<T>> {
+    fn join_all(self) -> Vec<thread::Result<T>>{
+        let all:Vec<_> = self.into_iter()
+            .map(move |v| v.into().join())
+            .collect();
+        return all;
     }
 }
 
